176ca058d779fe587f696aa8d8b42d76acc0a754
nihad-mammadli <bynihadmammadli@gmail.com>
2024-09-05 20:47:16 +0400

    Initial Commit

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by Nihad on 05.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
